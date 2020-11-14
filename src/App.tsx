a418e20a43f3c463e786b3e922e4b2063937dee0
Amine OUCHIHA <ouchihaamine@gmail.com>
2020-11-15 00:46:37 +0100

    feat(navigation): add tab navigation

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,36 @@ import 'react-native-gesture-handler';
 import React from "react";
 import Search from "./components/Search";
 import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
 import FilmDetail from './components/filmDetail/FilmDetail';
 import { Provider } from 'react-redux'
 import Store from './redux/configureStore'
+import Favorites from './components/Favorites';
 
 const Stack = createStackNavigator();
+
+function Navigation() {
+  return (
+    <Stack.Navigator initialRouteName="Search">
+      <Stack.Screen name="Search"
+        options={{ title: 'Recherche' }} component={Search} />
+      <Stack.Screen name="FilmDetail"
+        options={{ title: 'Detail du film' }} component={FilmDetail} />
+    </Stack.Navigator>
+  );
+}
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <Provider store={Store}>
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="Search">
-          <Stack.Screen name="Search"
-            options={{ title: 'Recherche' }} component={Search} />
-          <Stack.Screen name="FilmDetail"
-            options={{ title: 'Detail du film' }} component={FilmDetail} />
-        </Stack.Navigator>
+        <Tab.Navigator>
+          <Tab.Screen name="Films" options={{ title: 'Films' }} component={Navigation} />
+          <Tab.Screen name="Favoris" options={{ title: 'Favoris' }} component={Favorites} />
+        </Tab.Navigator>
       </NavigationContainer>
     </Provider>
   );
